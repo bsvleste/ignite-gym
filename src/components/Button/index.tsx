2373be0ea8cf2631bb3eec733509bf3c4747c620
Bruno <bvaleiro@gmail.com>
2023-04-22 16:01:43 -0300

    feature: add component button and finishing screen sigin

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import { Button as NativeBaseButton, IButtonProps, Text } from 'native-base'
+type ButtonProps = IButtonProps & {
+  title: string
+  variant?: 'solid' | 'outline'
+}
+
+export function Button({ title, variant = 'solid', ...props }: ButtonProps) {
+  return (
+    <NativeBaseButton
+      w='full'
+      h={14}
+      bg={variant === "outline" ? "transparent" : 'green.700'}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor='green.500'
+      rounded='md'
+      _pressed={{
+        bg: variant === "outline" ? 'gray.500' : 'green.500',
+        borderWidth: variant === 'outline' ? 0 : 1
+      }}
+      {...props}
+    >
+      <Text
+        color={variant === "outline" ? 'green.500' : 'white'}
+        fontFamily="heading"
+        fontSize="sm"
+      >{title}</Text>
+    </NativeBaseButton>
+  )
+}
--- src/screens/SigiIn/index.tsx
+++ src/screens/SigiIn/index.tsx
@@ -1,43 +1,63 @@
-import { VStack, Image, Text, Center, Heading } from 'native-base'
+import { Platform, KeyboardAvoidingView } from 'react-native'
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 import BackgroundOImg from '@assets/background.png'
 import GymLogoSVG from '@assets/logo.svg'
 import { Input } from '@components/Input'
+import { Button } from '@components/Button'
 export function SignIn() {
   return (
-    <VStack
-      px={5}
-      flex={1}
-      bg='gray.700'
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
     >
-      <Image
-        position="absolute"
-        resizeMode='contain'
-        alt='pessoal malhando'
-        source={BackgroundOImg}
-      />
-      <Center my={24}>
-        <GymLogoSVG />
-        <Text
-          color='gray.100'
-          fontSize='sm'
-          fontFamily='body'>
-          Treine sua mente e seu corpo
-        </Text>
-      </Center>
-      <Center>
-        <Heading
-          color="gray.100"
-          fontSize='xl'
-          mb={6}
-          fontFamily='heading'>
-          Acesse sua conta
-        </Heading>
-      </Center>
-      <Input
-        placeholder='E-mail'
-        keyboardType='email-address'
-        autoCapitalize='none' />
-      <Input placeholder='Password' />
-    </VStack>
+      <VStack
+        px={5}
+        flex={1}
+        bg='gray.700'
+        pb={16}
+      >
+        <Image
+          position="absolute"
+          resizeMode='contain'
+          alt='pessoal malhando'
+          source={BackgroundOImg}
+        />
+        <Center my={16}>
+          <GymLogoSVG />
+          <Text
+            color='gray.100'
+            fontSize='sm'
+            fontFamily='body'>
+            Treine sua mente e seu corpo
+          </Text>
+        </Center>
+        <Center>
+          <Heading
+            color="gray.100"
+            fontSize='xl'
+            mb={6}
+            fontFamily='heading'>
+            Acesse sua conta
+          </Heading>
+        </Center>
+        <Center>
+          <Input
+            placeholder='E-mail'
+            keyboardType='email-address'
+            autoCapitalize='none' />
+          <Input placeholder='Password' secureTextEntry />
+          <Button title='Acessar' />
+        </Center>
+        <Center mt={24}>
+          <Text
+            color='gray.100'
+            fontFamily='body'
+            fontSize='sm'
+            mb={3}>
+            Ainda não Tem Acesso?</Text>
+          <Button title='Acessar' variant='outline' />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
